f21e43d0b516a5328a90122fc0b5e3712f831466
Saket Kumar Jha <callmesaket@gmail.com>
2022-01-07 01:44:21 +0530

    Add files via upload

---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9aoFFXfKb6Q5n-_xIYRLfX9_6mpLJaHw",
+    authDomain: "mygitapp-7d4de.firebaseapp.com",
+    projectId: "mygitapp-7d4de",
+    storageBucket: "mygitapp-7d4de.appspot.com",
+    messagingSenderId: "70631517285",
+    appId: "1:70631517285:web:30aa371bcb3acd993daa8c"
+  };
+
+  export default firebaseConfig
